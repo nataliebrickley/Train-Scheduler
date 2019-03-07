6eeb25da0124cb58d64b1b775560c77642a899de
Natalie Brickley <natt_rose@yahoo.com>
2019-03-06 19:29:10 -0800

    Added files and links

---
+++ app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDbe98c-uPp571CdWjBvVjoKRi2wM7bja8",
+    authDomain: "train-scheduler-70b27.firebaseapp.com",
+    databaseURL: "https://train-scheduler-70b27.firebaseio.com",
+    projectId: "train-scheduler-70b27",
+    storageBucket: "",
+    messagingSenderId: "591577837073"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
